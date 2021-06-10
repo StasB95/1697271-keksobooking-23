cf59f9384444c091aef4cbaf0faaa8dc69615afc
StasB95 <1995bsa@gmail.c0m>
2021-06-10 22:56:05 +0300

    Нужно больше функций(Правки)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 // Функция №1
-function number(min, max) {
+const getNumberInRange = (min, max) => {
   if (min >= 0 && max >= 0) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-}
-number();
-
+};
+getNumberInRange();
 
 // Функция №2
-function coordinate(min, max, decimalPlaces) {
+const getFloatNumberInRange = (min, max, decimaPlaces) => {
   if (min >= 0 && max >= 0 && min < max) {
-    return ((Math.random() * (max - min + 1)) + min).decimalPlaces(decimalPlaces);
+    return ((Math.random() * (max - min + 1)) + min).toFixed(decimaPlaces);
   }
 }
-coordinate();
+getFloatNumberInRange();
